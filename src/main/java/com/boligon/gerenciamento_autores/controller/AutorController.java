b81aaa4a5ae614b60633bb98c1d66ef1817152fb
Filipe Boligon <filiperuizboligon9@gmail.com>
2026-03-17 16:17:47 -0300

    feat: criando controller do autor

---
+++ src/main/java/com/boligon/gerenciamento_autores/controller/AutorController.java
@@ -0,0 +1,47 @@
+package com.boligon.gerenciamento_autores.controller;
+
+import com.boligon.gerenciamento_autores.entity.AutorEntity;
+import com.boligon.gerenciamento_autores.service.AutorService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/autores")
+public class AutorController {
+
+        @Autowired
+    private AutorService autorService;
+
+    @PostMapping
+    public ResponseEntity<AutorEntity> save(@RequestBody AutorEntity novoAutor) {
+        AutorEntity autor = autorService.save(novoAutor);
+        return ResponseEntity.status(201).body(autor);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorEntity>> findAll() {
+        List<AutorEntity> autorList = autorService.findAll();
+        return autorList.isEmpty()
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.ok().body(autorList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AutorEntity> findById(@PathVariable Long id) {
+        Optional<AutorEntity> autor = autorService.findById(id);
+        return autor.isPresent()
+                ? ResponseEntity.ok().body(autor.get())
+                : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteById(Long id) {
+        autorService.deleteById(id);
+        return ResponseEntity.status(204).build();
+    }
+}
